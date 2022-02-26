28824844745fb1011d6ab48ccc8bb278f09cae4c
AlexanderShche <Neegroom@yandex.ru>
2022-02-26 04:05:14 +0300

    fix: adding a lot of tests

--- add-tests.bash
+++ add-tests.bash
@@ -11,14 +11,14 @@ do
     echo "enter the ${i} test:"
     read test
     count=$(ls -l tests/ | grep "^-" | wc -l)
-    j=$(($count+$i))
+    j=$(($count+1))
     touch tests/t${j}
     echo ${test} > tests/t${j}
     echo "test t${j} is added"
     echo "enter answer to this test:"
     read answer
     count=$(ls -l answers/ | grep "^-" | wc -l)
-    j=$(($count+$i))
+    j=$(($count+1))
     touch answers/a${j}
     echo ${answer} > answers/a${j}
     echo "answer a${j} is added"
